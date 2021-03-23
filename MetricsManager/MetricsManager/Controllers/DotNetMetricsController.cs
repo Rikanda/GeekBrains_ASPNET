c3168fbab4a14b0a37bd87722c99a3cf4e4b5a7c
verm-v <verm-v@yandex.ru>
2021-03-23 16:14:52 +0600

    Добавлены аналогичные заглушки методов для остальных контроллеров. Добавлено перечисление перцентилей.

--- MetricsManager/MetricsManager/Controllers/DotNetMetricsController.cs
+++ MetricsManager/MetricsManager/Controllers/DotNetMetricsController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using MetricsManager.Enums;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -11,5 +12,39 @@ namespace MetricsManager.Controllers
 	[ApiController]
 	public class DotNetMetricsController : ControllerBase
 	{
+		[HttpGet("agent/{agentId}/from/{fromTime}/to/{toTime}")]
+		public IActionResult GetMetricsFromAgent(
+			[FromRoute] int agentId,
+			[FromRoute] TimeSpan fromTime,
+			[FromRoute] TimeSpan toTime)
+		{
+			return Ok();
+		}
+
+		[HttpGet("agent/{agentId}/from/{fromTime}/to/{toTime}/percentiles/{percentile}")]
+		public IActionResult GetMetricsByPercentileFromAgent(
+			[FromRoute] int agentId,
+			[FromRoute] TimeSpan fromTime,
+			[FromRoute] TimeSpan toTime)
+		{
+			return Ok();
+		}
+
+		[HttpGet("cluster/from/{fromTime}/to/{toTime}")]
+		public IActionResult GetMetricsFromAllCluster(
+			[FromRoute] TimeSpan fromTime,
+			[FromRoute] TimeSpan toTime)
+		{
+			return Ok();
+		}
+
+		[HttpGet("cluster/from/{fromTime}/to/{toTime}/percentiles/{percentile}")]
+		public IActionResult GetMetricsByPercentileFromAllCluster(
+			[FromRoute] TimeSpan fromTime,
+			[FromRoute] TimeSpan toTime,
+			[FromRoute] Percentile percentile)
+		{
+			return Ok();
+		}
 	}
 }
