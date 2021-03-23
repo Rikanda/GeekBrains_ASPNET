7df2f57d720ed22488373c6dece1ed36314873d9
verm-v <verm-v@yandex.ru>
2021-03-23 16:23:12 +0600

    Добавлен котроллер управления агентами с заглушками методов

---
+++ MetricsManager/MetricsManager/Controllers/AgentsController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MetricsManager.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class AgentsController : ControllerBase
+	{
+		//Отладочный метод для проверки сервера
+		[HttpGet("read")]
+		public IActionResult Read()
+		{
+			return Ok("Запуск прошел успешно");
+		}
+
+		[HttpPost("register")]
+		public IActionResult RegisterAgent([FromBody] AgentInfo agentInfo)
+		{
+			return Ok();
+		}
+
+		[HttpPut("enable/{agentId}")]
+		public IActionResult EnableAgentById([FromRoute] int agentId)
+		{
+			return Ok();
+		}
+
+		[HttpPut("disable/{agentId}")]
+		public IActionResult DisableAgentById([FromRoute] int agentId)
+		{
+			return Ok();
+		}
+	}
+	
+	public class AgentInfo
+	{
+		public int AgentId { get; set; }
+		public Uri AgentAddress { get; set; }
+	}
+}
