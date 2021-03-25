b1eb3421dc1724a373f550111ed768e228e20923
Verm-V <Verm-V@yandex.ru>
2021-03-26 00:11:56 +0600

    Добавил логгер Nlog в менеджер метрик с простейшим конфигом

--- MetricsManager/MetricsManager/Program.cs
+++ MetricsManager/MetricsManager/Program.cs
@@ -1,4 +1,7 @@
+using System;
+using NLog.Web;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Hosting;
 
 namespace MetricsManager
@@ -7,14 +10,38 @@ namespace MetricsManager
 	{
 		public static void Main(string[] args)
 		{
+			var logger = NLogBuilder.ConfigureNLog("nlog.config").GetCurrentClassLogger();
+			try
+			{
+				logger.Debug("---- [BEGIN]----");
+				logger.Debug("init main");
+				CreateHostBuilder(args).Build().Run();
+			}
+			// отлов всех исключений в рамках работы приложения
+			catch (Exception exception)
+			{
+				//NLog: устанавливаем отлов исключений
+				logger.Error(exception, "Stopped program because of exception");
+				throw;
+			}
+			finally
+			{
+				// остановка логера 
+				NLog.LogManager.Shutdown();
+			}
+
 			CreateHostBuilder(args).Build().Run();
 		}
 
 		public static IHostBuilder CreateHostBuilder(string[] args) =>
-			Host.CreateDefaultBuilder(args)
-				.ConfigureWebHostDefaults(webBuilder =>
-				{
-					webBuilder.UseStartup<Startup>();
-				});
+			Host.CreateDefaultBuilder(args).ConfigureWebHostDefaults(webBuilder =>
+			{
+				webBuilder.UseStartup<Startup>();
+			})
+			.ConfigureLogging(logging =>
+			{
+				logging.ClearProviders(); // создание провайдеров логирования
+				logging.SetMinimumLevel(LogLevel.Trace); // устанавливаем минимальный уровень логирования
+			}).UseNLog(); // добавляем библиотеку nlog
 	}
 }
