0301d0107a75d5e0a6b217aafb0db9943a5c9a7a
Verm-V <Verm-V@yandex.ru>
2021-04-18 22:11:34 +0600

    Добавлена заготовка WPF проекта, для демонстрации метрик собранных Менеджером метрик

---
+++ MetricsManager/MetricsManagerClient/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Windows;
+
+namespace MetricsManagerClient
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void Button_Click(object sender, RoutedEventArgs e)
+		{
+			//CpuChart.ColumnServiesValues[0].Values.Add(48d);
+			CpuChart.ColumnServiesValues[0].Values.Add(new Random().NextDouble()*100);
+			CpuChart.ColumnServiesValues[0].Values.RemoveAt(0);
+		}
+
+		private void CpuChart_Loaded(object sender, RoutedEventArgs e)
+		{
+
+		}
+	}
+}
